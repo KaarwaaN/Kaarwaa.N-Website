dc3ab3c19df8e7c3590ce1454c6b7f70d3dc5423
ARYAN TYAGI <130476378+Aryan3007@users.noreply.github.com>
2023-10-14 19:46:58 +0530

    aryan

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,22 @@
+import { BsArrowUpRight } from "react-icons/bs";
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+    <div className="h-20 w-full flex flex-row justify-between items-center px-32">
+        <ul className="flex text-[#24387a] font-semibold justify-start items-center gap-8">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Our Work</li>
+          <li>Gallery</li>
+          <li>Contact Us</li>
+        </ul>
+        <div className="flex flex-row justify-center items-center gap-2">
+    <input type="search" className=" py-1  w-64 border-slate-200 border-2 rounded-full" />
+        <button className="border-2 flex flex-row justify-center items-center gap-2 font-semibold text-[#24387a] px-4 py-1 rounded-full border-[#24387a]">Donate Now <BsArrowUpRight/></button>
+        </div>
+    </div>
+    
+    </>
   )
 }
 
